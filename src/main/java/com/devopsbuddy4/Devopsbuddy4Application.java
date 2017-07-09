67e1a554e2f34dbc0ae76b70e86ea94c49479b88
Khetho Borole <kbborole2@gmail.com>
2017-07-09 16:02:46 -0700

    First commit for Spring boot project

---
+++ src/main/java/com/devopsbuddy4/Devopsbuddy4Application.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopsbuddy4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devopsbuddy4Application.class, args);
+	}
+}
